695b699b2f1b855c227e117d8399791d0dee687d
Matheus Muniz <mmuniz605@gmail.com>
2023-09-14 09:36:15 -0300

    feat: add challenge GrandeDeposito

---
+++ src/main/java/com/mtmuniz/GrandeDeposito.java
@@ -0,0 +1,19 @@
+package com.mtmuniz;
+
+import java.util.Scanner;
+
+public class GrandeDeposito {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double valor = scanner.nextDouble();
+
+        if (valor > 0) {
+            System.out.printf("Deposito realizado com sucesso! \nSaldo atual: R$ %.2f" , valor);
+        } else if (valor < 0) {
+            System.out.println("Valor invalido! Digite um valor maior que zero.");
+        } else {
+            System.out.println("Encerrando o programa...");
+        }
+    }
+}
